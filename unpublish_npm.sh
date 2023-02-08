51c851e5972edcf321ab440f966c6b7b63c9d469
luran <luran@xd.com>
2023-02-08 15:37:52 +0800

    [chore] update to 1.0.4

--- unpublish_npm.sh
+++ unpublish_npm.sh
@@ -3,13 +3,6 @@ rootPath=$(pwd)
 
 npm_source_dic_root="$rootPath"/Assets/TapTap/AndroidDependencyResolver
 
-# 多个模块一起发布
-# npm_module_name=("Mainland")
-npm_module_name=("Account" "Common" "Payment" "Oversea" "Mainland")
-npm_module_package_name=("com.xd.sdk.account" "com.xd.sdk.common" "com.xd.sdk.payment" "com.xd.sdk.oversea" "com.xd.sdk.mainland")
-
-# 单个模块发布
-#npm_module_name=("Account")
 
 unpublishNPM() {
   echo email=qiankun@xd.com > .npmrc
@@ -17,7 +10,7 @@ unpublishNPM() {
   echo registry=https://nexus.tapsvc.com/repository/npm-registry/ >> .npmrc
   echo //nexus.tapsvc.com/repository/npm-registry/:_authToken=NpmToken.de093789-9551-3238-a766-9d2c694f2600 >> .npmrc
   
-  npm unpublish "com.tapsdk.androiddependencyresolver"@"$2"
+  npm unpublish "com.tapsdk.androiddependencyresolver"@"$1"
   
   rm -rf .npmrc
   
